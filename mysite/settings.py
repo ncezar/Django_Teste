47b4357ad4a3173d9c7bb45895bd337d40da0105
ncezar <nathalia.lcz@hotmail.com>
2019-01-31 21:00:45 -0200

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,11 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pedido',
-<<<<<<< 9f9ffdd18c4322d695dfc86a253abb00b4c8b198
-    
-=======
 
->>>>>>> erros finais corrigidos
 
 ]
 
@@ -54,13 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-<<<<<<< 9f9ffdd18c4322d695dfc86a253abb00b4c8b198
-<<<<<<< HEAD
 
-=======
->>>>>>> 702232158b50a7bce461d0541373c00e570ba281
-=======
->>>>>>> erros finais corrigidos
+
 ]
 
 ROOT_URLCONF = 'mysite.urls'
@@ -76,14 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-<<<<<<< 9f9ffdd18c4322d695dfc86a253abb00b4c8b198
-<<<<<<< HEAD
 
-=======
->>>>>>> 702232158b50a7bce461d0541373c00e570ba281
-=======
-
->>>>>>> erros finais corrigidos
             ],
         },
     },
@@ -138,24 +122,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-<<<<<<< 9f9ffdd18c4322d695dfc86a253abb00b4c8b198
-<<<<<<< HEAD
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-=======
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-#STATICFILES_DIRS = (
-#    os.path.join(PROJECT_ROOT, 'static'),
-#)
->>>>>>> 702232158b50a7bce461d0541373c00e570ba281
-=======
+
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
->>>>>>> erros finais corrigidos
+
