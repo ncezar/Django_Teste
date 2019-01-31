6f65776fadcf0fcabdce8eaca2914982d842eff5
Nathalia <nathalia.lcz@hotmail.com>
2019-01-30 23:21:55 -0200

    rentabilidades corretas na pasta

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pedido',
-    
+
 
 ]
 
@@ -50,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
 ]
 
 ROOT_URLCONF = 'mysite.urls'
--- pedido/forms.py
+++ pedido/forms.py
@@ -7,13 +7,6 @@ class CadastroForm(forms.ModelForm):
         model = Cadastro
         fields = ['cliente', 'item', 'preco_unitario', 'quantidade']
         #---#
-    def clean_preco_unitario(self):
-        preco_unitario = self.cleaned_data['preco_unitario']
-        if preco_unitario <= 0:
-            raise forms.ValidationError("O preço precisa ser maior que 0!")
-        return self.cleaned_data
-    #---#
-
 
     def clean(self):
         item = self.cleaned_data['item']
@@ -21,7 +14,9 @@ class CadastroForm(forms.ModelForm):
         preco_unitario = self.cleaned_data['preco_unitario']
         #preco_unitario = item.preco_unit
         print('Item.Preco_unit',item.preco_unit)
-        print('Preco_Unit',preco_unitario)#item ja é objeto de Produto, logo ele alcança os valores dos campos desta tabela, logo é só comprar com a quantidade
+        print('Preco_Unit',self.cleaned_data['preco_unitario'])#item ja é objeto de Produto, logo ele alcança os valores dos campos desta tabela, logo é só comprar com a quantidade
+        if preco_unitario <= 0:
+            raise forms.ValidationError("O preço precisa ser maior que 0!")
         if quantidade <= 0:
             raise forms.ValidationError("A quantidade precisa ser maior que 0!")
         if (({'preco_unitario': preco_unitario}) < ((item.preco_unit)*0.9)):
